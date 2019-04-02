c0d37aa6283a1b592536fb05eea6ea7aef1f3f8a
Hung Tran <hung@0x0.ninja>
2019-04-02 07:21:17 -0500

    add error handler for discord.js

--- bot.js
+++ bot.js
@@ -1,11 +1,9 @@
 const client = require('./client')
 const plugins = require('./lib/plugins')
 
-client.on('ready', () => {
-  // eslint-disable-next-line no-console
-  console.log(`Logged in as ${client.user.tag}`)
-})
-
+// eslint-disable-next-line no-console
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}`))
+// eslint-disable-next-line no-console
+client.on('error', error => console.error(`WS encountered an error: ${error}`))
 client.on('message', plugins)
-
 client.login(process.env.DISCORD_TOKEN)
